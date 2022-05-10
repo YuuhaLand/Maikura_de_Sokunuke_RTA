926dd12ffc47a4fde6f74c36e08dcabdc75f4030
ごて優香 Yuka0745 <78265558+Gotemba912@users.noreply.github.com>
2022-05-10 19:04:04 +0900

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,33 @@
+{
+  "format_version": 2,
+  "header": {
+    "name": "即抜けRTA",
+    "description": "即抜けを検知して通知します!",
+    "version": [ 1, 0, 0 ],
+    "uuid": "cc541bc1-3a8d-4018-a778-56f2640eb488",
+    "min_engine_version": [ 1, 13, 0 ]
+  },
+  "modules": [
+    {
+      "type": "javascript",
+      "description": "",
+      "version": [ 1, 0, 0 ],
+      "uuid": "dbb2c27c-29bd-497f-8a08-3275d674bf96",
+      "entry": "scripts/Main.js"
+    }
+  ],
+  "dependencies": [
+    {
+      "uuid": "6f4b6893-1bb6-42fd-b458-7fa3d0c89616",
+      "version": [ 0, 1, 0 ]
+    },
+    {
+      "uuid": "b26a4d4c-afdf-4690-88f8-931846312678",
+      "version": [ 0, 1, 0 ]
+    },
+    {
+      "uuid": "2bd50a27-ab5f-4f40-a596-3641627c635e",
+      "version": [ 0, 1, 0 ]
+    }
+  ]
+}
